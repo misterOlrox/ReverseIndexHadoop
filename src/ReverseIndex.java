38da288b5e434c993aa53763cfab2f50b58d000f
Mister Olrox <nikitakholdobin@yandex.ru>
2020-12-19 03:04:43 +0200

    reverse indexes

--- src/ReverseIndex.java
+++ src/ReverseIndex.java
@@ -1,12 +1,12 @@
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
@@ -16,35 +16,49 @@ import org.apache.hadoop.util.ToolRunner;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Set;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 public class ReverseIndex extends Configured implements Tool {
 
+    public final static String SEPARATOR = "\t";
+
     public static class Map
-            extends Mapper<LongWritable, Text, Text, IntWritable> {
-        private final static IntWritable one = new IntWritable(1);
+            extends Mapper<LongWritable, Text, Text, Text> {
         private Text word = new Text();
 
-        public void map(LongWritable key, Text value, Context context)
-                throws IOException, InterruptedException {
+        @Override
+        public void map(
+                LongWritable key,
+                Text value,
+                Context context
+        ) throws IOException, InterruptedException {
+            FileSplit fileSplit = (FileSplit) context.getInputSplit();
+            String filename = fileSplit.getPath().getName();
+
             String line = value.toString();
             StringTokenizer tokenizer = new StringTokenizer(line);
             while (tokenizer.hasMoreTokens()) {
                 word.set(tokenizer.nextToken());
-                context.write(word, one);
+                context.write(word, new Text(filename));
             }
         }
     }
 
     public static class Reduce
-            extends Reducer<Text, IntWritable, Text, IntWritable> {
-        public void reduce(Text key, Iterable<IntWritable> values,
-                           Context context) throws IOException, InterruptedException {
-            int sum = 0;
-            for (IntWritable val : values) {
-                sum += val.get();
+            extends Reducer<Text, Text, Text, Text> {
+        @Override
+        public void reduce(
+                Text key,
+                Iterable<Text> values,
+                Context context
+        ) throws IOException, InterruptedException {
+            Set<String> texts = new TreeSet<>();
+            for (Text val : values) {
+                texts.add(val.toString());
             }
-            context.write(key, new IntWritable(sum));
+            context.write(key, new Text(String.join(SEPARATOR, texts)));
         }
     }
 
@@ -53,7 +67,7 @@ public class ReverseIndex extends Configured implements Tool {
         job.setJarByClass(ReverseIndex.class);
         job.setJobName("wordcount");
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
+        job.setOutputValueClass(Text.class);
         job.setMapperClass(Map.class);
         job.setCombinerClass(Reduce.class);
         job.setReducerClass(Reduce.class);
